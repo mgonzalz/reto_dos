95684b96e45ef9447a34fe41e88d891bb5cfc451
BobiStudio <mgonzgarc@myuax.com>
2023-09-24 12:32:29 +0200

    corrección

--- ejercicio03.py
+++ ejercicio03.py
@@ -1,29 +1,33 @@
 t = int(input())
-
-def funcion(subs, online, conexiones):
-    if subs == online:
-        print('YES')
-    else:
-        for i in conexiones:
-            if i == '+':
-                subs -=1
-                online +=1
-            elif i == '-':
-                online -=1
+while not t == 0:
+    def funcion(subs, online, conexiones):
         if subs == online:
-            print('MAYBE')
-        elif subs > online:
-            print('NO')
+            return 'YES'
         else:
-            print('YES')
+            online_conexiones = 0
+            online_desconexiones = 0
+            for i in conexiones:
+                if i == '+':
+                    online_conexiones +=1
+                elif i == '-':
+                    online_desconexiones -=1
+                elif subs == online + online_conexiones + online_desconexiones:
+                    return 'YES'
+            if subs == online + online_conexiones + online_desconexiones:
+                return 'YES'
+            elif online_conexiones >= subs - online:
+                return 'MAYBE'
+            else:
+                return 'NO'
 
 
-n,a,q = map(int, input().split())
-conexiones = list(map(str, input()))
-while not len(conexiones) == q:
-    print('Error, ingrese nuevamente las conexiones')
+    n,a,q = map(int, input().split())
     conexiones = list(map(str, input()))
+    while not len(conexiones) == q:
+        print('Error, ingrese nuevamente las conexiones')
+        conexiones = list(map(str, input()))
+    t=t-1
 
-funcion(n,a,conexiones)
+    print(funcion(n,a,conexiones))
 
 
